6b8b7cb86b2ae7ea54eea9750cdd06fd2d2dda65
Lars Lokaizyk <lars.lokaizyk@appsfactory.de>
2017-01-22 11:19:46 +0100

    [ADD] android library to display joke from joke lib;

--- app/src/main/java/com/udacity/gradle/builditbigger/MainActivityFragment.java
+++ app/src/main/java/com/udacity/gradle/builditbigger/MainActivityFragment.java
@@ -6,11 +6,12 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Toast;
 
 import com.example.Joker;
 import com.google.android.gms.ads.AdRequest;
 import com.udacity.gradle.builditbigger.databinding.FragmentMainBinding;
+
+import de.lokaizyk.jokepresenter.JokeActivity;
 
 
 /**
@@ -38,6 +39,6 @@ public class MainActivityFragment extends Fragment {
     }
 
     public void tellJoke() {
-        Toast.makeText(getContext(), Joker.tellJoke(), Toast.LENGTH_SHORT).show();
+        JokeActivity.displayJoke(getContext(), Joker.tellJoke());
     }
 }
---
+++ jokepresenter/src/main/java/de/lokaizyk/jokepresenter/JokeActivity.java
@@ -0,0 +1,40 @@
+package de.lokaizyk.jokepresenter;
+
+import android.content.Context;
+import android.content.Intent;
+import android.databinding.DataBindingUtil;
+import android.databinding.ObservableField;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import de.lokaizyk.jokepresenter.databinding.ActivityJokeBinding;
+
+public class JokeActivity extends AppCompatActivity {
+
+    public ObservableField<String> joke = new ObservableField<>();
+
+    private static final String EXTRA_JOKE = "keyExtraJoke";
+
+    public static void displayJoke(Context context, String joke) {
+        if (context == null) {
+            return;
+        }
+        Intent intent = new Intent(context, JokeActivity.class);
+        if (joke != null) {
+            intent.putExtra(EXTRA_JOKE, joke);
+        }
+        context.startActivity(intent);
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        ActivityJokeBinding binding = DataBindingUtil.setContentView(this, R.layout.activity_joke);
+        binding.setJokeActivity(this);
+
+        Bundle extras = getIntent().getExtras();
+        if (extras != null && extras.containsKey(EXTRA_JOKE)) {
+            joke.set(extras.getString(EXTRA_JOKE));
+        }
+    }
+}
