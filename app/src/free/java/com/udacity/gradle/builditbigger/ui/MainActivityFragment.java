39df9aa500a0a638a862802f3f240fdf24605eca
Lars Lokaizyk <lars.lokaizyk@appsfactory.de>
2017-01-22 15:50:45 +0100

    [ADD] show interstitial ad in free version;

--- app/src/free/java/com/udacity/gradle/builditbigger/ui/MainActivityFragment.java
+++ app/src/free/java/com/udacity/gradle/builditbigger/ui/MainActivityFragment.java
@@ -8,7 +8,9 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.android.gms.ads.AdListener;
 import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.InterstitialAd;
 import com.udacity.gradle.builditbigger.R;
 import com.udacity.gradle.builditbigger.databinding.FragmentMainBinding;
 import com.udacity.gradle.builditbigger.network.JokeTask;
@@ -20,6 +22,8 @@ import de.lokaizyk.jokepresenter.JokeActivity;
  * A placeholder fragment containing a simple view.
  */
 public class MainActivityFragment extends Fragment implements JokeTask.JokeListener{
+
+    InterstitialAd mInterstitialAd;
 
     public ObservableBoolean isLoading = new ObservableBoolean(false);
 
@@ -38,17 +42,44 @@ public class MainActivityFragment extends Fragment implements JokeTask.JokeListe
         AdRequest adRequest = new AdRequest.Builder()
                 .addTestDevice(AdRequest.DEVICE_ID_EMULATOR)
                 .build();
+
+        mInterstitialAd = new InterstitialAd(getContext());
+        mInterstitialAd.setAdUnitId("ca-app-pub-3940256099942544/1033173712");
+        mInterstitialAd.setAdListener(new AdListener() {
+            @Override
+            public void onAdClosed() {
+                loadJoke();
+                requestNewInterstitial();
+            }
+        });
+        requestNewInterstitial();
+
         binding.adView.loadAd(adRequest);
         return binding.getRoot();
     }
 
     public void tellJoke() {
+        if (mInterstitialAd.isLoaded()) {
+            mInterstitialAd.show();
+        } else {
+            loadJoke();
+        }
+    }
+
+    private void loadJoke() {
         isLoading.set(true);
         new JokeTask().execute(this);
     }
 
+    private void requestNewInterstitial() {
+        AdRequest adRequest = new AdRequest.Builder()
+                .addTestDevice(AdRequest.DEVICE_ID_EMULATOR)
+                .build();
+        mInterstitialAd.loadAd(adRequest);
+    }
+
     @Override
-    public void jokeLoaded(String joke) {
+    public void jokeLoaded(final String joke) {
         isLoading.set(false);
         JokeActivity.displayJoke(getContext(), joke);
     }
